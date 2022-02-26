65351b789f5bbad2e50d5a4f66d110e841bf8f4e
gp1108 <pietrogirotto00@gmail.com>
2022-02-26 10:49:12 +0100

    first commit

---
+++ Solution27.java
@@ -0,0 +1,10 @@
+public class Solution27 {
+  public static int removeElement(int[] nus, int val) {
+    System.out.println("hello");
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    removeElement(new int[4], 4);
+  }
+}
