2b66fe1de9160e0e8d84f16ff4732d08c468fec7
gp1108 <pietrogirotto00@gmail.com>
2022-02-26 11:19:58 +0100

    Problem 27

--- Solution27.java
+++ Solution27.java
@@ -1,10 +1,26 @@
-public class Solution27 {
-  public static int removeElement(int[] nus, int val) {
-    System.out.println("hello");
-    return 0;
-  }
+/*
+  Author: gp-1108
+  Time: O(n)
+  Space: O(1)
+  Score: Faster than 100%, Memory less than 44.97%
+*/
 
-  public static void main(String[] args) {
-    removeElement(new int[4], 4);
+public class Solution27 {
+  public static int removeElement(int[] nums, int val) {
+    int k = 0;
+        while(k < nums.length && nums[k] != val) {
+            k++;
+        }
+        int i = k+1;
+        while(i < nums.length) {
+            if(nums[i] == val) {
+                i++;
+                continue;
+            }
+            nums[k] = nums[i];
+            i++;
+            k++;
+        }
+        return k;
   }
 }
