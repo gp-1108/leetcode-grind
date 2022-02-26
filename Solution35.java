7ffa0eb467507602249587d1e76b0cdd6a5fcc97
gp1108 <pietrogirotto00@gmail.com>
2022-02-26 17:26:47 +0100

    Problem35

---
+++ Solution35.java
@@ -0,0 +1,27 @@
+/**
+ * Author: gp-1108
+ * Time: O(log n)
+ * Space: O(log n)
+ * Score: 100%, 41.21%
+ */
+public class Solution35 {
+  public int searchInsert(int[] nums, int target) {
+    if(nums.length <= 0) return 0;
+    return binSearch(nums, target, 0, nums.length-1);
+  }
+    
+  private int binSearch(int[] nums, int target, int left, int right) {
+    int mid = (left+right)/2;
+    
+    if(nums[mid] == target) return mid;
+    
+    if(left >= right && nums[left] > target) return left;
+    if(left >= right && nums[left] < target) return left+1;
+    
+    if(nums[mid] > target) {
+      return binSearch(nums, target, left, mid-1);
+    } else {
+      return binSearch(nums, target, mid+1, right);
+    }
+  }
+}
